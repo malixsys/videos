12a3feec1a29f357c0aceeb30b9dce2d8415bf45
malixsys <github@malix.com>
2016-04-25 23:22:03 -0400

    fixing 500

--- app.js
+++ app.js
@@ -51,24 +51,12 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    console.error( {
-      message: err.message,
-      error: err
-    });
-    res.end(500);
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
+  console.error( {
     message: err.message,
-    error: {}
+    error: err
   });
+  res.end(500);
 });
 
 
